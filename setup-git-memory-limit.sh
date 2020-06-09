1b01a3798b54df7f20a18b6e33c74ade1c23f7c0
liyang <7325560@qq.com>
2020-06-09 09:11:39 +0800

    add setup git memory limitation script add setup npm mirror script

---
+++ setup-git-memory-limit.sh
@@ -0,0 +1,5 @@
+git config --global core.packedgitlimit 128m
+git config --global core.packedgitwindowsize 128m
+git config --global pack.deltacachesize 128m
+git config --global pack.packsizelimit 128m
+git config --global pack.windowmemory 128m
